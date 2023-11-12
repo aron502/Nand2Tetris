33f33866ace993ae92b0a484af41a595ebcc3b88
AronYoung <tkf6fkt@gmail.com>
2023-11-12 19:30:02 +0800

    finish proj04

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,65 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+    // max = 24575
+    @24575
+    D=A
+    @max
+    M=D
+
+    // cur = SCREEN
+    @SCREEN
+    D=A
+    @cur
+    M=D
+
+(LOOP)
+    @KBD
+    D=M
+    @FILL
+    D;JGT
+    @CLEAR
+    0;JMP
+
+(FILL)
+    @max
+    D=M
+    @cur
+    D=D-M
+
+    @LOOP
+    D;JLT
+
+    @cur
+    A=M
+    M=-1
+
+    @cur
+    M=M+1
+
+    @LOOP
+    0;JMP
+(CLEAR)
+    @SCREEN
+    D=A
+    @cur
+    D=D-M
+    @LOOP
+    D;JGT
+
+    @cur
+    A=M
+    M=0
+
+    @cur
+    M=M-1
+
+    @LOOP
+    0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,36 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+//     r2 = 0
+// LOOP:
+//     if (r1 == 0) goto END
+//     r2 += r0
+//     r1 -= 1
+//     goto LOOP
+// END:
+
+@R2
+M=0
+
+(LOOP)
+
+    @R1
+    D=M
+    @END
+    D;JEQ
+
+    @R0
+    D=M
+    @R2
+    M=M+D
+
+    @R1
+    M=M-1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
